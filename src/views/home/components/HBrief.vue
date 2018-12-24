1739c6a16f017edc801a486d5a81efb5d88bedfb
jq <18710609128@163.com>
2018-12-24 18:12:05 +0800

    首页完善渲染信息

--- src/views/home/components/HBrief.vue
+++ src/views/home/components/HBrief.vue
@@ -2,17 +2,17 @@
     <div class="brief">
         <div class="brief-left"></div>
         <div class="brief-right"></div>
-        <div class="content">
+        <div class="content" :style="{'background-image':'url('+data.bgPhoto+')'}">
             <div class="container clear">
                 <div class="left l-wraper">
-                    <h1>{{data.advertTitle}}</h1>
-                    <h2>introduction of group company</h2>
+                    <h1>{{data.companyName}}</h1>
+                    <h2>{{data.companyEnglishName}}</h2>
                     <div class="cpy-img">
-                        <img :src="data.advertPicUrl" alt="">
+                        <img :src="data.mainPhoto" alt="">
                     </div>
                 </div>
                 <div class="left r-wraper">
-                    <p>翔天世纪（西安）集团有限公司始建于2017年，是专注于专业服务房地产行业的民营企业公司，公司坐落西安市雁塔区太白南路上上国际2301室，主要经营企业管理咨询服务，翔天世纪（西安）集团有限公司以始终坚持“诚信为本、客户至上、互惠互利，开拓创新”的企业宗旨，本着“本质为本、专业执着、精益求精”的经营销售理念，力求给客户提供全方位优质服务，我们相信诚实正直、开拓进取地为公司发展做正确的事情，将为公司和个人带来共同的利益和进步。</p>
+                    <p>{{data.content||'翔天世纪（西安）集团有限公司始建于2017年，是专注于专业服务房地产行业的民营企业公司，公司坐落西安市雁塔区太白南路上上国际2301室，主要经营企业管理咨询服务，翔天世纪（西安）集团有限公司以始终坚持“诚信为本、客户至上、互惠互利，开拓创新”的企业宗旨，本着“本质为本、专业执着、精益求精”的经营销售理念，力求给客户提供全方位优质服务，我们相信诚实正直、开拓进取地为公司发展做正确的事情，将为公司和个人带来共同的利益和进步。'}}</p>
                 </div>
                 <div class="line"></div>
             </div>
@@ -34,7 +34,11 @@ export default {
     overflow: hidden;
     height: 526px;
     position: relative;
-    .bgCover(url('../../../../static/images/brief-bg.jpg'));
+    // .bgCover(url('../../../../static/images/brief-bg.jpg'));
+    background-size: cover;
+    background-position:center center;
+    background-repeat:no-repeat;
+
     background-attachment: fixed;
     .brief-left,
     .brief-right{
--- src/views/home/components/HCase.vue
+++ src/views/home/components/HCase.vue
@@ -5,10 +5,10 @@
             <h2>企业服务宣传标语，尽量体现出服务特色</h2>
             <ul class="item">
                 <li v-for="(item,idx) in data" :key="idx">
-                    <img :src="item.advertPicUrl">
+                    <img :src="item.mainPhoto">
                     <div class="infro">
-                        <h3>{{item.advertTitle||'服务案例名称'}}</h3>
-                        <p>想居住的目标从未改变。在此基础上，“三好”体系再次深入理解人居需求，让好房子、好服务、好社区 的理念传递......  。。。。多家庭。万科“三好”，是万科始终如一的坚持，更是追求美好生活的信念</p>
+                        <h3>{{item.projectName||'服务案例名称'}}</h3>
+                        <p>{{item.content||'想居住的目标从未改变。在此基础上，“三好”体系再次深入理解人居需求，让好房子、好服务、好社区 的理念传递......  。。。。多家庭。万科“三好”，是万科始终如一的坚持，更是追求美好生活的信念'}}</p>
                     </div>
                 </li>
             </ul>
